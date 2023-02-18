3e3cb6adb107cd7a74a55d7fe0e938c07d49cfbc
Richard Dallaway <richard@dallaway.com>
2023-02-18 18:21:17 +0000

    add failing test for media

--- src/markdown.rs
+++ src/markdown.rs
@@ -87,6 +87,12 @@ date: 2023-02-04T21:22:20+00:00
 --
 
 <p>A visit to the ASMR exhibit at the Design Museum. Yes, of course there was a Bob Ross room (as part of the unintentional ASMR section of the exhibit).</p><p><a href="https://designmuseum.org/exhibitions/weird-sensation-feels-good-the-world-of-asmr" target="_blank" rel="nofollow noopener noreferrer"><span class="invisible">https://</span><span class="ellipsis">designmuseum.org/exhibitions/w</span><span class="invisible">eird-sensation-feels-good-the-world-of-asmr</span></a></p>
+
+![An area partly enclosed by cream coloured soft walls, bending in like hands or waves. There are three large TV screens showing ASMR works. Groups of people, in twos and threes, sit on the soft cushioning wearing headsets, the cables stretching to the screens. No one is wearing shoes.](https://files.mastodon.green/media_attachments/files/109/808/524/409/930/443/original/38da0da8e5badfdc.jpeg)
+
+![An entry in the programme, describing a video: “ASMR can be triggered by watching someone explain something.
+In 1988, the Icelandic artist Björk was the lead singer of The Sugarcubes. In this film, which was part of a documentary made about the band, Bork describes how a television works. Her softly spoken description is accompanied by a close-up shot of her finger tracing the inside of a cathode-ray tube television set. 
+The success of an ASM work is often determined by the relationship a viewer has to the ASMRtist through the screen. In this film, Björk's explanation is both generous and empathetic.”](https://files.mastodon.green/media_attachments/files/109/808/524/733/756/242/original/4f4f643fae86839f.jpeg)
 "#;
 
         assert_eq!(expected, item.as_markdown().unwrap());
